0136a083d94cc7b19d515d6e95aeaa356ba46be4
ArtemPetyalin <a.petyalin@school.nsu.ru>
2024-10-13 12:01:17 +0700

    Здравствуйте! Доделал 3 задание

--- task9.py
+++ task9.py
@@ -2,3 +2,30 @@
 # Определить минимальное и максимальное среди двузначных чисел,
 # которые делятся на 3. Если таких чисел не было, вывести "нет".
 
+N = int(input())
+num_list = []
+
+if N > 0:
+
+    for i in range(N):
+        a = int(input())
+
+        if abs(a) > 9 and abs(a) < 100 and a % 3 == 0:
+            num_list.append(a)
+
+
+if len(num_list) == 0:
+    print('нет')
+
+else:
+    min, max = num_list[0], num_list[0]
+    for i in range(len(num_list)):
+
+        if num_list[i] < min:
+            min = num_list[i]
+
+        elif num_list[i] > max:
+            max = num_list[i]
+
+print('Минимальное двузначное число, делящееся на 3: ' + str(min))
+print('Максимальное двузначное число, делящееся на 3: ' + str(max))
